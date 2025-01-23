c7b74a3958f4eae178cd1e01df11e1feeb08f620
cloudflypeng <cloudflypeng@gmail.com>
2025-01-23 20:31:35 +0800

    chore: init

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/seo', '@nuxt/ui', '@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#3B82F6',
+            secondary: '#10B981',
+            // 您可以根据需要添加更多自定义颜色
+          }
+        }
+      }
+    }
+  }
+})
